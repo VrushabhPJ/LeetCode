af153d59d05b7c926681ec8d52c996b80189fa8d
vrushabh jadhav <vrushabh.jadhavvpj@gmail.com>
2023-11-17 17:24:43 +0530

    reverse linked list

---
+++ Linked_List/ReverseLinkedList.java
@@ -0,0 +1,42 @@
+/* Reverse Link List II
+ Reverse a linked list A from position B to C.
+
+NOTE: Do it in-place and in one-pass.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+
+public class Solution {
+    public ListNode reverseBetween(ListNode A, int B, int C) {
+        if(A.next == null || (B == C)) return A;  //if A next null or B & C are same
+
+        ListNode dummy= new ListNode(0);
+        dummy.next= A;
+
+        int pos= 1;
+        ListNode nodebefore= dummy;
+        while(pos < B) {
+            nodebefore= nodebefore.next;
+            pos++;
+        }
+        ListNode workingptr= nodebefore.next; // start of sublist at B point to reverse part of list
+        while(B < C) {
+            ListNode nodeExtract= workingptr.next;
+            workingptr.next= nodeExtract.next;
+
+            nodeExtract.next= nodebefore.next;
+            nodebefore.next= nodeExtract;
+            B++;
+        }
+
+        return dummy.next;
+    }
+}
